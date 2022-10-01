8d1b8000bb860b0e4bc1663485ecdd522f6807bd
Dabeen-Yun <90141741+Dabeen-Yun@users.noreply.github.com>
2022-10-02 03:54:32 +0900

    관심분야 설정

--- pages/login/occupation.tsx
+++ pages/login/occupation.tsx
@@ -8,8 +8,15 @@ interface jobs {
   backdrop_image: string;
 }
 
+interface interests {
+  id: number;
+  title: string;
+  backdrop_image: string;
+}
+
 function Occupation() {
   const [list, setList] = useState<jobs[]>([]);
+  const [interest, setInterest] = useState<interests[]>([]);
 
   useEffect(() => {
     axios
@@ -20,15 +27,23 @@ function Occupation() {
       .catch(function (error) {
         console.log(error);
       });
+    axios
+      .get("http://cashup.iptime.org:8000/polls/fields/")
+      .then((response) => {
+        setInterest([...response.data]);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }, []);
 
   return (
     <div>
       <MainNav title={"logo"} />
-      <div className="w-[350px] m-auto rounded-xl bg-[#FFFFFF] shadow-xl mt-5">
+      <div className="w-[350px] m-auto rounded-xl bg-[#FFFFFF] shadow-xl mt-5 pb-5">
         <div className="p-5">
           <span className="font-bold text-[16px]">
-            나의 희망직종(다중선택 가능){" "}
+            나의 희망직종(다중선택 가능)
           </span>
         </div>
         <div className="grid grid-cols-2 gap-[10px] px-5">
@@ -36,9 +51,32 @@ function Occupation() {
             return (
               <div className="rounded-xl relative" key={index}>
                 <img src={job.backdrop_image} width={150} height={150} />
-                <span className="absolute text-white text-[20px] font-bold z-10 flex justify-center items-center top-[66px]">
-                  {job.title}
-                </span>
+                <div className="flex justify-center items-center">
+                  <span className="absolute text-white text-[20px] font-bold z-10 top-[66px]">
+                    {job.title}
+                  </span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="w-[350px] m-auto rounded-xl bg-[#FFFFFF] shadow-xl mt-5 pb-5 mb-[100px]">
+        <div className="p-5">
+          <span className="font-bold text-[16px]">
+            나의 관심분야(다중선택 가능)
+          </span>
+        </div>
+        <div className="grid grid-cols-2 gap-[10px] px-5">
+          {interest.map((interest, index) => {
+            return (
+              <div className="rounded-xl relative" key={index}>
+                <img src={interest.backdrop_image} width={150} height={150} />
+                <div className="flex justify-center items-center">
+                  <span className="absolute text-white text-[20px] font-bold z-10 top-[66px]">
+                    {interest.title}
+                  </span>
+                </div>
               </div>
             );
           })}
