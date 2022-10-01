45e890104b4f7d884de72638d82406b8c9d8eddd
jaehyeon_park <janhyun99@naver.com>
2022-10-01 16:01:13 +0900

    설정

--- components/menu.tsx
+++ components/menu.tsx
@@ -9,7 +9,7 @@ interface mainProps {
 function Menu({ title, des, src }: mainProps) {
   return (
     <>
-      <div className="bg-white w-[170px] h-[170px] rounded-lg p-5 mt-4">
+      <div className="bg-white w-[170px] h-[170px] rounded-lg p-5 mt-[10px] sho">
         <div>
           <h2 className="text-[18px] font-bold">{title}</h2>
           <p className="text-[#767676] text-[14px]">{des}</p>
